779871de842e0a49369d1690d3700afec4e4aae8
Matthew Jaikaran <mattjaikaran@gmail.com>
2018-03-21 19:35:08 -0400

    add database connection

--- server.js
+++ server.js
@@ -13,10 +13,8 @@ const image = require('./controllers/image')
 const db = knex({
   client: 'pg',
   connection: {
-    host : 'postgresql-cylindrical-33254',
-    user : 'mattjaikaran',
-    password : '',
-    database : 'smart-brain'
+    host : process.env.DATABASE_URL,
+    ssl: true,
   }
 })
 
